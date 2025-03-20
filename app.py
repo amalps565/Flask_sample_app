af36978049e4563378cee2c131554609f614e331
amalps565 <amal.p@edstem.com>
2025-03-20 11:30:43 +0530

    flask migerate

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def create_app(db_url=None):
         return (
             jsonify(
                 {
-                    "description": "Request does not contain an access token.",
+                    "description": "Request does not contain an access  token.",
                     "error": "authorization_required",
                 }
             ),
